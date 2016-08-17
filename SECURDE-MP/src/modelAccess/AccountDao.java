e47c694452633d3ba77d89c0a206ade73c793d69
regalabianca <regalabianca@gmail.com>
2016-08-17 22:25:33 +0800

    LOL naiwan from last push HAHAHA

--- SECURDE-MP/src/modelAccess/AccountDao.java
+++ SECURDE-MP/src/modelAccess/AccountDao.java
@@ -5,6 +5,7 @@ import models.Account;
 public interface AccountDao {
 	public boolean checkIfUsernameExists (String username);
 	public boolean isPasswordValid (String password);
+	public int getType (int accountId);
 	public boolean addAccount (Account account, String password);
 	public Account getAccount (int accountId);
 	public Account getAccount (String username);
