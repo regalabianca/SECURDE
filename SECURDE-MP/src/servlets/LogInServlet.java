cb1c42f043bd9abb1c7bc1c5366099f7d8d8b8f2
janinetan <janine_tan14@yahoo.com>
2016-08-19 17:19:33 +0800

    Invalidating the user session before creating

--- SECURDE-MP/src/servlets/LogInServlet.java
+++ SECURDE-MP/src/servlets/LogInServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import database.DBManager;
 import modelAccess.AccountDao;
@@ -36,18 +37,22 @@ public class LogInServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		if(request.getSession() != null){
-			request.getSession().invalidate();
-			response.sendRedirect("account.jsp");
-		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
+		
+		if(session != null)
+			session.invalidate();
+		
+		session = request.getSession(true);
+		
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
+				
 		String genpass;
 		Password pass = new Password();
 	
